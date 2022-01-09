1a104d417b20e51483e3790ee73c92a955021074
enesgutic <enesguto@gmail.com>
2022-01-09 16:00:33 +0100

    Added Gallery on homepage

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
-
+import lightbox from '../node_modules/lightbox2/dist/js/lightbox'
 
 jQuery(() => {
-  
   // Code goes here
-
+  lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'alwaysShowNavOnTouchDevices': true
+  })
 
 })
 
