09932716dd721ba9929b9d3cf374076772661d95
enesgutic <enesguto@gmail.com>
2022-01-10 01:21:36 +0100

    Added About page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import $ from 'jquery'
 import jQuery from 'jquery'
 import '../public/styles/style.scss'
+import '../public/styles/about.scss'
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
@@ -15,5 +16,14 @@ jQuery(() => {
     'alwaysShowNavOnTouchDevices': true
   })
 
-})
+  // Active link
+
+// $(document).ready(function(){
+//   $('ul li a').click(function(){
+//     $('li a').removeClass("active");
+//     $(this).addClass("active");
+// })
+// })
+
+}) // END jQuery
 
